37308a87b6bc89bcd9cfd44a317e62d1cc6fe903
Thangaraju R <thangarajube@gmail.com>
2020-04-01 09:44:11 +0530

    reverted back the changes on SP

--- terraform/azure-rbac.tf
+++ terraform/azure-rbac.tf
@@ -1,17 +1,5 @@
-data "azurerm_subscription" "primary" {
-}
-
-data "azurerm_client_config" "sp-aks" {
-}
-
 resource "azurerm_role_assignment" "sp-aks-network" {
     scope                   = "${azurerm_virtual_network.demo.id}"
     role_definition_name    = "Network Contributor"
     principal_id            = "${azuread_service_principal.sp-aks.object_id}"
-}
-
-resource "azurerm_role_assignment" "sp-aks" {
-  scope                = data.azurerm_subscription.primary.id
-  role_definition_name = "Contributor"
-  principal_id         = "${azuread_service_principal.sp-aks.object_id}"
 }
